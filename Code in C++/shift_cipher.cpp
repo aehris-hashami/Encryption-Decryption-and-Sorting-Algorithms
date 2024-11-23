d42dfcaa2eb10a8138211a52c01d9ef5ec5ea96a
Aehris Hashami <aehris.hashami@gmail.com>
2024-11-23 19:06:31 +0500

    Comments and debuging is completed for shift cipher.

--- Code in C++/shift_cipher.cpp
+++ Code in C++/shift_cipher.cpp
@@ -1,26 +1,53 @@
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 
-string shift_cipher
-(string plaintext, int shift)
-{
-    string ciphertext = ""; 
-    shift %= 26;
-    for(auto i:plaintext) 
-        ciphertext += char(int(i) + shift);
+
+
+// utility functions
+vector<int> convert_letters_to_num(string);
+
+// encryption and decryption functions
+string encrypt(string,int);
+string decrypt(string,int);
+
+
+
+int main(){
+
+    cout << "Enter messege: "; string txt; getline(cin,txt);
+    cout << "Enter shift key: "; int key; cin >> key;
+    
+    cout << "The encrypted text is: " << encrypt(txt,key) << endl;
+
+    cout << "The decrypted text is: " << decrypt(encrypt(txt,key),key);
+
+    return 0;
+}
+
+
+// encryption method
+
+vector<int> convert_letters_to_num(string text){
+    vector<int> vec;
+     
+    for(char i:text) vec.push_back((64 < int(i) && int(i) < 91)?(int(i)-65):(int(i)-97));
+    
+    return vec;
+}
+
+string encrypt(string plaintext, int shift){
+    auto str_vec = convert_letters_to_num(plaintext); string ciphertext = "";
+
+    for(auto i:str_vec){i = (i+shift)%26 + 65; ciphertext += char(i);}  
+    
     return ciphertext;
 }
 
-int main(){
-    cout << "Enter messege: "; 
-    string txt; getline(cin,txt);
-    
-    cout << "Enter shift key: "; 
-    int key; cin >> key;
-    
-    string ctxt = shift_cipher(txt,key);
-    cout << "The encrypted text is: " << ctxt;
 
-    return 0;
+// decryption method
+
+string decrypt(string ciphertext, int shift){
+    return encrypt(ciphertext,(-1)*shift);
 }
