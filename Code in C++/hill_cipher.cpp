72b21c67c7db89a1abc3835ec56ecebcb48b64b9
Mubashir78 <mubashirrindbaloch@gmail.com>
2024-11-28 13:48:58 +0500

    Issue: Invertebale matarix cannot be produced due the nature of the integer value devision resulting in ceal value, therefore causing the value to round off to  zero.

--- Code in C++/hill_cipher.cpp
+++ Code in C++/hill_cipher.cpp
@@ -25,17 +25,29 @@ string decrypt(vector<int>,vector<vector<int>>);
 
 
 int main(){
-
+    // generating and checking transformational matrix
     vector<vector<int>>  keymatrix = gen_square_matrix(4); 
     if(determinant(keymatrix) == 0) return 0; else cout << "Invertebale matrix is slected" << endl;
     for(auto i:keymatrix){for(auto j:i) cout << j << ' '; cout << endl;} cout << endl;
 
+    // sample plaintext and testing encryption function
     string plaintext = "abcdefgh"; cout << "Plain text: " << plaintext << endl;
     vector<int> cipher_vec = encrypt(plaintext,keymatrix); 
+
+    // displaying encrypted values
+    cout << "Encryted letter values: ";
+    for(auto i:cipher_vec) cout << i << ' '; cout << endl;
     
-    string ciphertext = "";
+    // constructing cipher text
+    string ciphertext = ""; for(auto i:cipher_vec) ciphertext += char(i%26 + 65);
     cout << "Cipher text: " << ciphertext << endl;
-    cout << "After decryption: " << decrypt(cipher_vec,keymatrix) << endl;
+
+    // checking inverted matrix
+    vector<vector<int>> invmat = inverse_matrix(keymatrix);
+    for(auto i:invmat){for(auto j:i) cout << j << ' '; cout << endl;} cout << endl;
+
+    // decrypting the encrypted cipher text
+    // cout << "After decryption: " << decrypt(cipher_vec,keymatrix) << endl;
 
     return 0;
 }
@@ -154,13 +166,8 @@ vector<vector<int>> inverse_matrix(vector<vector<int>>& mat) {
     vector<vector<int>> adj = adjugate_matrix(mat);
     vector<vector<int>> inverse(n, vector<int>(n));
     
-    // for (int i = 0; i < n; ++i) {
-    //     for (int j = 0; j < n; ++j) {
-    //         cout << "inverse[" << i << "][" << j << "] = " << inverse[i][j] << endl;
-
-    //         inverse[i][j] = adj[i][j] / det;
-    //     }
-    // }
+    for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) inverse[i][j] = adj[i][j] / det;
+         
     return inverse;
 }
 
@@ -180,17 +187,18 @@ vector<vector<int>> sharding(vector<int> vec, int lps){
 string decrypt(vector<int> ciphertext, vector<vector<int>> mat){
     // get the inverse matrix
     vector<vector<int>> inverse_mat = inverse_matrix(mat);
-    for(auto i:inverse_mat) for(auto j:i){cout << j << ' '; cout << endl;} cout << endl;
+    //for(auto i:inverse_mat) for(auto j:i){cout << j << ' '; cout << endl;} cout << endl;
 
     // get individual vectors and apply transformation by inverse matrix
     vector<vector<int>> cipher_vec = sharding(ciphertext,inverse_mat.size());
 
-    // 
+    // applying transformation to all the cipher vectors
     vector<vector<int>> plain_vec; 
     for(auto i:cipher_vec) plain_vec.push_back(apply_transformation(inverse_mat,i));
 
+    // concatenating all the characters into one string
     string plaintext = "";
-    for(int i=0; i<cipher_vec.size(); i++) for(int j=0; j<cipher_vec[i].size(); j++) 
-        plaintext += char(plain_vec[i][j]);
+    for(int i=0; i<plain_vec.size(); i++) for(int j=0; j<plain_vec[i].size(); j++) 
+        plaintext += char(plain_vec[i][j] + 65);
     return plaintext;
 } 
